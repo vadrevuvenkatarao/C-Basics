0a3fc5925c8a9b9198d3314a4b30d35fd0b1205e
vadrevuvenkatarao <92287883+vadrevuvenkatarao@users.noreply.github.com>
2021-10-20 10:27:43 +0530

    Add files via upload

---
+++ use of float int car.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	// variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	//initialization
+	age = 18;
+	cgpa =8.09;
+	initial ='B'; // character should be enclosed using single quotes
+	//output
+	printf("%d",age);
+	printf("%f",cgpa);
+	printf("%c", initial);
+}
+      
