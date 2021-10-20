c41faf48e6843dded5f9c2a5e40bf2576668e51a
vadrevuvenkatarao <92287883+vadrevuvenkatarao@users.noreply.github.com>
2021-10-20 09:53:44 +0530

    Add files via upload

---
+++ sum of twom numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int number1, number2, sum;
+	//initialization
+	number1=1634;
+	number2=3416;
+	//calculation
+	sum=number1+number2;
+	//output
+	printf("sum of two numbers %d", sum);
+}
