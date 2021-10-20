cbbc42cd1ba123e35cec55e9f9596a764a69b74d
vadrevuvenkatarao <92287883+vadrevuvenkatarao@users.noreply.github.com>
2021-10-20 09:59:01 +0530

    Add files via upload

---
+++ difference of two numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int number1, number2, difference;
+	//initialization
+	number1=1634;
+	number2=3416;
+	//calculation
+	difference=number1+number2;
+	//output
+	printf("difference of two numbers %d", difference);
+}
